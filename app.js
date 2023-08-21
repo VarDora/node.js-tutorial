f8a0be509236c57db3b979dca868b73836dc5bef
Varga Dora <dory0224@gmail.com>
2023-08-21 17:01:26 +0200

    stream

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-const { writeFileSync } = require('fs')
-for (let i = 0; i < 10000; i++) {
-  writeFileSync('./content/big.txt', `hello world ${i}\n`, { flag: 'a' })
-}
+var http = require('http')
+var fs = require('fs')
+
+http
+  .createServer(function (req, res) {
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
+    // res.end(text)
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
+    fileStream.on('open', () => {
+      fileStream.pipe(res)
+    })
+    fileStream.on('error', (err) => {
+      res.end(err)
+    })
+  })
+  .listen(5000)
